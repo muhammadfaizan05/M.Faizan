6db49f6ca31cc0966f6182e13584bcc86aace2d5
Muhammad Faizan <faizan3154@gmail.com>
2022-03-31 22:16:36 +0500

    Uzair Birthday

---
+++ docs/script.js
@@ -0,0 +1,12 @@
+var a="Muhammad Faizan";
+ var b="tiddi";
+  	if(a== "Muhammad Faizan" && b=="tiddi")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
